2e0e60ae11be4a6e34bbb28c9d6ea91c84265e3e
chinnadurai06 <ssdurai06@gmail.com>
2025-09-10 18:22:44 +0530

    first commit

--- Q2.java
+++ Q2.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 public class Q2 {
     public static void main(String[] args) {
         Scanner hello=new Scanner(System.in);
-        int balance=hello.nextInt();
-        if(balance>0){
+        int balance=hello.nextInt();              //User input
+        if(balance>0){                              // Balance high
             System.out.println("Positive Balance");
         }
-        if(balance<0){
+        if(balance<0){                                //Balance Minus
             System.out.println("Overdraft");
         }
-        if(balance==0){
+        if(balance==0){                               // Balance Was Zero
             System.out.println("Zero Balance");
         }
         else{
