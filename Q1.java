788428b379695c56348d8d57f0c0b8d7670c1458
chinnadurai06 <ssdurai06@gmail.com>
2025-09-10 18:20:31 +0530

    first commit

--- Q1.java
+++ Q1.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 public class Q1 {
     public static void main(String[] args) {
        Scanner hello=new Scanner(System.in);
-       int n=hello.nextInt();
-       if(n%2==0){
+       int n=hello.nextInt();              //user input
+       if(n%2==0){                         //Even number syntax
            System.out.println("ON");
        }
        else{
