1a5a655b7550573907877db01048b1921b21b6c5
chinnadurai06 <ssdurai06@gmail.com>
2025-09-10 18:26:17 +0530

    first commit

--- Q4.java
+++ Q4.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 public class Q4 {
     public static void main(String[] args) {
         Scanner hello=new Scanner(System.in);
-        int a=hello.nextInt();
+        int a=hello.nextInt();              //User Inputs
         int b=hello.nextInt();
         int c=hello.nextInt();
-        if(a>=b && a>=c){
+        if(a>=b && a>=c){                    //Biggest Treasure
             System.out.println(""+a);
         }
         if(b>=a && b>=c){
