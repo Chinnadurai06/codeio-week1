e10541938a3af80edf44a8006df1c7e430ca0011
chinnadurai06 <ssdurai06@gmail.com>
2025-09-10 18:27:11 +0530

    first commit

--- Q5.java
+++ Q5.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class Q5 {
     public static void main(String[] args) {
         Scanner hello=new Scanner(System.in);
-        int year=hello.nextInt();
+        int year=hello.nextInt();             //User Inputs
         boolean years;
-        years=(year%4==0&&year%100!=0||year%400==0);
+        years=(year%4==0&&year%100!=0||year%400==0);  //LeapYear Formula
         if(years){
             System.out.println("Leap Year");
         }
